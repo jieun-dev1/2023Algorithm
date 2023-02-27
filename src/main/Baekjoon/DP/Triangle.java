17da5f0e4efd6d65b048d355837df785d1fc1eba
jieun-dev1 <jkdev1324@gmail.com>
2023-02-27 13:33:10 +0900

    문제 다시 품
    이전 문제에서 어려웠던 부분.
    -> 왼쪽 대각선/ 오른쪽 대각선만 참조 가능한 경우를 분기하는 것이었는데 그냥 가로 세로를 길이 하 더 길게 만들면 된다.
    정수 삼각형 역시 우리가 배열을 탐색할때, 1을 더 줘서 [-1][-1] 처럼해주면 0이 아닌 경우를 참조하는 걸 막아준다.
    
    시간 복잡도는 여전히 O(N^2)이나, 이전에 분기문 했던 코드 보다 훨씬 빠르다.

--- src/main/Baekjoon/DP/Triangle.java
+++ src/main/Baekjoon/DP/Triangle.java
@@ -1,11 +1,11 @@
 package main.Baekjoon.DP;
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-/**
- * 배열로 구현 후, 배열 위치와 비교하는 부분에서 막힘. 소요: 1시간 40분
- */
 public class Triangle {
 
   //초기화를 -1로 해두기. 5x5 배열로 만들지만, -1이면 읽을 수 없음
@@ -16,52 +16,37 @@ public class Triangle {
   public static void solution() {
 
     //O(N^2)
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        //양 옆이 모두 있을 경우.
-        if (i - 1 >= 0 && j - 1 >= 0 && j <= i - 1) {
-          arr[i][j] += Math.max(arr[i - 1][j - 1], arr[i - 1][j]);
-          continue;
-        }
-
-        //왼쪽만 가능할 겨웅
-        if (i - 1 >= 0 && j == i && j > i - 1) {
-          arr[i][j] += arr[i - 1][j - 1];
-          continue;
-        }
-
-        //대각선 오른쪽만 가능할 경우 (가장자리에 있음)
-        if (i - 1 >= 0 && j - 1 < 0 && j == 0) {
-          arr[i][j] += arr[i - 1][j];
-          continue;
-        }
-
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        arr[i][j] = Math.max(arr[i-1][j-1]+arr[i][j], arr[i-1][j]+arr[i][j]);
       }
     }
   }
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    n = sc.nextInt();
-    arr = new int[n][n];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine()); //공백단위로 읽을 수 있는 라인 추가
+    n = Integer.parseInt(st.nextToken()); //한줄에서 공백 단위로 읽음
+    arr = new int[n+1][n+1];
 
     //O(N^2)
     //outerloop: 0 - n-1 == n
     //innerloop: i+1
     //총 1+2+3+4+...+n; n*(n+1)/2 이지만, 상수 시간 1/2은 무시됨.
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        arr[i][j] = sc.nextInt();
+    for (int i = 1; i <= n; i++) {
+      st = new StringTokenizer(br.readLine()); //공백단위로 읽을 수 있는 라인 추가
+      for (int j = 1; j <= i; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
       }
     }
+
     solution();
 
     int answer = 0;
     //O(N) - 고려되지 않음.
-    for (int i = 0; i < n; i++) {
-      answer = Math.max(answer, arr[n - 1][i]);
+    for (int i = 1; i <=n; i++) {
+      answer = Math.max(answer, arr[n][i]);
     }
-
     System.out.println(answer);
   }
 }
